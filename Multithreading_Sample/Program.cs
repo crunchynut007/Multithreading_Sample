9bdadb2d578175f96eb90730822cdbc64a65bab2
crunchynut007 <kandiahmischel@gmail.com>
2018-06-19 18:01:57 +0930

    Initial Commit

--- Multithreading_Sample/Program.cs
+++ Multithreading_Sample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,44 @@ namespace Multithreading_Sample
 {
 	class Program
 	{
+		//static int[] dataset = new int[16];
+		static int[] dataset = { 1, 2, 5, 9, 29, 89, 149, 500 };
+		static long Totalsum = 0;
+
+		public static void PopulateArray(int[] array) {
+			Random rand = new Random();
+			for (int i = 0; i < array.Length; i++) {
+				array[i] = rand.Next(10);
+			}
+		}
+
+		public static long NonthreadedSum(int[] array) {
+			long sum = 0;
+			for (int i = 0; i < array.Length; i++) {
+				sum += array[i];
+			}
+			return sum;
+		}
+
 		static void Main(string[] args) {
+			//PopulateArray(dataset);
+			Stopwatch watch = new Stopwatch();
+			watch.Start();
+			Totalsum = NonthreadedSum(dataset);
+			watch.Stop();
+			Console.WriteLine(Totalsum);
+			Console.WriteLine(watch.Elapsed);
+			watch.Reset();
+
+			Totalsum = 0;
+			Summation Adder = new Summation();
+			watch.Start();
+			Totalsum = Adder.SumArray(dataset);
+			Console.WriteLine(value: Totalsum);
+			watch.Stop();
+			Console.WriteLine(watch.Elapsed);
+
+			Console.ReadKey();
 		}
 	}
 }
---
+++ Multithreading_Sample/Summation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreading_Sample
+{
+	class Summation
+	{
+		readonly int numberOfProcessors = Environment.ProcessorCount;
+		int[] portionResults = new int[Environment.ProcessorCount];
+		public int Totalsummings = 0;
+
+
+		public long SumArray(int[] data) {
+			int portionlength = data.Length / numberOfProcessors;
+			Thread[] threads = new Thread[numberOfProcessors];
+			for (int i = 0; i < numberOfProcessors - 1; i++) {
+				threads[i] = new Thread(() => Sumyourportion(i, data, portionlength));
+				threads[i].Start();
+
+			}
+			for (int i = 0; i < numberOfProcessors - 1; i++) {
+				threads[i].Join();
+
+			}
+			Totalsummings = SumAllPortions();
+			return Totalsummings;
+		}
+
+
+		private void Sumyourportion(int portionNumber, int[] array, int length) {
+			int subarraytotal = 0;
+			lock (array.SyncRoot) {
+				for (int i = portionNumber * length; i < (portionNumber + 1) * length; i++) {
+					subarraytotal += array[i];
+				}
+			}
+			lock (portionResults.SyncRoot) {
+				portionResults[portionNumber] = subarraytotal;
+			}
+
+		}
+
+
+		private int SumAllPortions() {
+			int totalsum = 0;
+
+			for (int i = 0; i < portionResults.Length; i++) {
+				totalsum += (int)portionResults[i];
+			}
+			return totalsum;
+		}
+	}
+}
